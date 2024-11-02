1e883ec6396a9142c7426a18b73cf13c487d656f
Mmacan2 <mariomacan2@gmail.com>
2024-11-02 11:05:39 +0100

    Added basic styling with bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express=require("express");
-
+const ejsMate=require("ejs-mate")
 const path=require('path');
 const mongoose=require("mongoose");
 const methodOverride=require("method-override")
@@ -16,6 +16,7 @@ db.once("open",()=>{
 });
 
 const app= express();
+app.engine("ejs", ejsMate);
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname,"views"))
